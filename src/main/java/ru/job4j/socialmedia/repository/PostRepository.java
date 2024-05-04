5577c730b80e06cab84e151024348e6db3f1545b
Boris <boris86msk@yandex.ru>
2024-05-04 12:17:15 +0300

    dell @Repository annotation

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,9 +1,7 @@
 package ru.job4j.socialmedia.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ru.job4j.socialmedia.model.Post;
 
-@Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
 }
--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package ru.job4j.socialmedia.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ru.job4j.socialmedia.model.User;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
