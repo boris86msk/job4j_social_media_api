5b36fe3076a9c232745b528e55efca3e1f8ce8bd
Boris <boris86msk@yandex.ru>
2024-05-29 22:36:56 +0300

    add native select to repository methods /checkstyle

--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -11,11 +11,11 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @Query("select u from User u where u.login = :login and u.password = :password")
     User findByLoginAndPassword(@Param("login") String login, @Param("password") String password);
 
-    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id" +
-            " where s.user_id = :id", nativeQuery = true)
+    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id"
+            + " where s.user_id = :id", nativeQuery = true)
     List<User> findAllSubscriptions(@Param("userId") Long id);
 
-    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id" +
-            " where s.user_id = :id and s.is_mutual = true", nativeQuery = true)
+    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id"
+            + " where s.user_id = :id and s.is_mutual = true", nativeQuery = true)
     List<User> findAllFriends(@Param("userId") Long id);
 }
