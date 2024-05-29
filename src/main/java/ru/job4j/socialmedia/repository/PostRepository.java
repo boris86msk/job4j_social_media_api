18603aec5e4e03ea242c7588ba02db40bc2531e3
Boris <boris86msk@yandex.ru>
2024-05-29 22:28:01 +0300

    add native select to repository methods

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,6 +1,9 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.Post;
 
 import java.time.LocalDateTime;
@@ -10,4 +13,22 @@ public interface PostRepository extends CrudRepository<Post, Long> {
     List<Post> findAllByUserId(int id);
     List<Post> findAllByCreatedGreaterThanEqualAndCreatedLessThanEqual(LocalDateTime from, LocalDateTime before);
     List<Post> findAllByOrderByCreatedDesc();
+    @Modifying(clearAutomatically = true)
+    @Query("update Post p set p.title = :title, p.text = :text where p.id = :id")
+    void updateTitleAndText(@Param("title") String title, @Param("text") String text, @Param("id ") Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Post p set p.filePath = null where p.id = :id")
+    void deletePostPicture(@Param("id") Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete Post p where p.id = :id")
+    void deletePostById(@Param("id") Long id);
+
+    @Query(value = "select p from posts p "
+            + "join users u on u.id = p.user_id "
+            + "join subscriptions s on u.id = s.subscribe_id "
+            + "where s.user_id = :userId "
+            + "order by p.created desc", nativeQuery = true)
+    List<Post> findUserSubscriptionsPosts(@Param("userId") Long id);
 }
--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -1,8 +1,21 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.List;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("select u from User u where u.login = :login and u.password = :password")
+    User findByLoginAndPassword(@Param("login") String login, @Param("password") String password);
+
+    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id" +
+            " where s.user_id = :id", nativeQuery = true)
+    List<User> findAllSubscriptions(@Param("userId") Long id);
+
+    @Query(value = "select u.login, u.email from users u join subscriptions s on u.id = s.subscribe_id" +
+            " where s.user_id = :id and s.is_mutual = true", nativeQuery = true)
+    List<User> findAllFriends(@Param("userId") Long id);
 }
