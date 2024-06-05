6a10ce328e6147754b414f154e261fa9f8f2c23b
boris86msk <boris86msk@yandex.ru>
2024-06-05 22:20:00 +0300

    dell whitespace

--- src/main/java/ru/job4j/socialmedia/service/PostService.java
+++ src/main/java/ru/job4j/socialmedia/service/PostService.java
@@ -3,7 +3,7 @@ package ru.job4j.socialmedia.service;
 import ru.job4j.socialmedia.model.Post;
 
 public interface PostService {
-    Post createPost (Post post);
-    void updatePost (Post post);
-    void deletePost (long id);
+    Post createPost(Post post);
+    void updatePost(Post post);
+    void deletePost(long id);
 }
